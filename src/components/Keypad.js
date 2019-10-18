ea2c58a3dee75a9953e265f472606575883862ad
Alan Krajina <alankrajina88@gmail.com>
2019-10-18 12:15:02 +0000

    Automatically backed up by Learn

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,10 +1,10 @@
 // Code Keypad Component Here
 export default class Keypad extends React.Component {
- 
+
   tickle = () => {
     console.log('Tee hee!');
   }
- 
+
   render() {
     return (
 		<button onClick={this.tickle}>Tickle me!</button>
